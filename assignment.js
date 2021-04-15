355ff675fb1b11339d4780c6f99a808c482c1aa9
Riaz Ahmed <ahmedriaz@engineer.com>
2021-04-15 07:48:53 +0600

    commit complete

--- assignment.js
+++ assignment.js
@@ -1,10 +1,8 @@
-// github repo Link: https://github.com/ahmedriaz-engineer/assignment-3
 
 // convert kilometerToMeter
-
 function kilometerToMeter(kilo) {
     if (kilo < 0) {
-        return "Distance is always positive";
+        return "Distance will always Positive"
     }
     else {
         var meter = kilo * 1000;
@@ -14,62 +12,61 @@ function kilometerToMeter(kilo) {
 
 
 // budgetCalculator Problem
-
-function budgetCalculator(watch, phone, laptop) {
-    if (watch < 0 || phone < 0 || laptop < 0) {
-        return "Enter Valid input which is not negative";
+function budgetCalculator(watch, mobile, laptop) {
+    if (watch < 0 || mobile < 0 || laptop < 0) {
+        return "Enter valid input which is not negative"
     }
     else {
         var watchPrice = watch * 50;
-        var phonePrice = phone * 100;
+        var mobilePrice = mobile * 100;
         var laptopPrice = laptop * 500;
-        var totalAmount = watchPrice + phonePrice + laptopPrice;
-        return totalAmount;
+        var budget = watchPrice + mobilePrice + laptopPrice;
+        return budget;
     }
 }
 
 
-// hotel cost calculator
-
-function hotelCost(day) {
-    var totalCost = 0;
-    if (day <= 10) {
-        totalCost = day * 100;
+//hotelCost Calculate
+function hotelCost(days) {
+    var cost = 0;
+    var first10DayCost = 10 * 100;
+    var second10DayCost = 10 * 80;
+    if (days < 0) {
+        return "Your staying days can't be a negative number."
     }
-    if (day < 1) {
-        return "It needs minimum 1day to stay at hotel, so enter valid day";
+    else if (days <= 10) {
+        var cost = days * 100;
     }
-    else if (day <= 20) {
-        var first10Day = 10 * 100;
-        var remaining = day - 10;
-        var second10Day = remaining * 80;
-        totalCost = first10Day + second10Day;
+    else if (days <= 20) {
+        var remainingDay = days - 10;
+        var remainingDayCost = remainingDay * 80;
+        var cost = first10DayCost + remainingDayCost;
     }
     else {
-        var first10Day = 10 * 100;
-        var second10Day = 10 * 80;
-        var remaining = day - 20;
-        var After20Day = remaining * 50;
-        totalCost = first10Day + second10Day + After20Day;
+        var remainingDay = days - 20;
+        var remainingDayCost = remainingDay * 50;
+        var cost = first10DayCost + second10DayCost + remainingDayCost;
     }
-    return totalCost;
+    return cost;
 }
 
 
-//Largest friend Name
-
-function megaFriend(name) {
-    var nameLength = 0;
-    var largeName;
-    if (name.length == 0) {
-        return "Enter some name or word in array";
+//find megaFriend
+function megaFriend(friendsName) {
+    var largeName = friendsName[0];
+    if (Array.isArray(friendsName) == false && friendsName.length == 0) {
+        return "You have to give string into an array or please input some value";
     }
-
-    for (var i = 0; i < name.length; i++) {
-        if (name[i].length > nameLength) {
-            var nameLength = name[i].length;
-            largeName = name[i];
+    else {
+        for (let i = 0; i < friendsName.length; i++) {
+            const name = friendsName[i];
+            if (typeof name !== "string") {
+                return "You have to input string value into the array"
+            }
+            else if (name.length > largeName.length) {
+                largeName = name;
+            }
         }
+        return largeName;
     }
-    return largeName;
 }
